94f2a3c46e553fd3f3c155f4134b9d56e1aae312
Shalitha Suranga <shalithas@99x.lk>
2019-03-26 12:56:05 +0530

    Update tsconfig etc

--- handler.ts
+++ handler.ts
@@ -2,13 +2,14 @@ import { APIGatewayProxyHandler } from 'aws-lambda';
 import User from './models/User';
 
 export const hello: APIGatewayProxyHandler = async (event, _context) => {
-  console.log(event);
   let response = funcLogic();
   return {
     statusCode: 200,
     body: response,
   };
 }
+
+
 
 function funcLogic(): any {
   let user = new User('Mike', 'John');
